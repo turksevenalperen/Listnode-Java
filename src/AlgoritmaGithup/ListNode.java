e705fb3b24a2eb4f2847d22133810f3614708b4b
turksevenalperen <127687857+turksevenalperen@users.noreply.github.com>
2024-12-10 14:06:42 +0300

    Add files via upload

---
+++ src/AlgoritmaGithup/ListNode.java
@@ -0,0 +1,20 @@
+package AlgoritmaGithup;
+
+
+public class ListNode {
+    public int data;
+    public ListNode next;
+
+    public ListNode() {
+        this(0, null);
+    }
+
+    public ListNode(int data) {
+        this(data, null);
+    }
+
+    public ListNode(int data, ListNode next) {
+        this.data = data;
+        this.next = next;
+    }
+}
